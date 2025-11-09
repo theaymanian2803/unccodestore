06dde015837d53c63cb15cab940f45736cbcd02c
aymane <atbohos@gmail.com>
2025-11-09 21:05:32 +0100

    opt

--- frontend/src/components/Grid.jsx
+++ frontend/src/components/Grid.jsx
@@ -3,7 +3,7 @@ import ProductGrid from './ProductGrid'
 
 function Grid({ products }) {
   return (
-    <div className=" mt-8 p-7 grid grid-cols-4 gap-4 max-w-7xl mx-auto  ">
+    <div className=" mt-8 p-7 grid grid-cols-4 gap-4">
       {products.map((product) => (
         <ProductGrid key={product._id} product={product} />
       ))}
